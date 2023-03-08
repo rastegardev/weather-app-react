2c5fa83bab4daef6f301ae0f6c5b04b9f695a748
reza rastegar <moidreza82@gmail.com>
2023-03-08 20:25:39 +0330

    The search page has been updated

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -9,6 +9,9 @@ import styled from "styled-components";
 // react hot toast
 import toast, { Toaster } from "react-hot-toast";
 
+// images
+import loadingGif from "../assets/img/loading.gif";
+
 const SearchContainer = styled.div`
   width: 100%;
   height: 90vh;
@@ -35,6 +38,7 @@ const SearchBox = styled.div`
     outline: none;
     text-align: center;
     font-size: 1.5rem;
+    border: 1px solid #f4f4f4;
     padding: 1.5rem 2.5rem;
     margin-top: 1.5rem;
     border-radius: 50px;
@@ -56,6 +60,9 @@ const SearchBox = styled.div`
     background: #feb248;
     border-radius: 50px;
     box-shadow: 0px 5px 10px -3px rgba(0, 0, 0, 0.1);
+    img {
+      width: 1.5rem;
+    }
     @media screen and (max-width: 768px) {
       padding: 1rem 2rem;
     }
@@ -63,7 +70,7 @@ const SearchBox = styled.div`
 `;
 const SearchData = styled.div`
   margin-top: 1.5rem;
-  padding: 1rem 2rem;
+  padding: 3rem 2rem;
   background: #f4f4f4;
   border-radius: 20px;
   box-shadow: 0px 5px 10px -3px rgba(0, 0, 0, 0.1);
@@ -72,11 +79,11 @@ const SearchData = styled.div`
   justify-content: space-between;
   .temp {
     font-weight: 700;
-    font-size: 2.5rem;
+    font-size: 5rem;
   }
   @media screen and (max-width: 768px) {
     .temp {
-      font-size: 2rem;
+      font-size: 4rem;
     }
   }
 `;
@@ -86,18 +93,22 @@ const apiKey = "75b9ff91c1c9b1d204bc50b6ee7bccda";
 const Search = () => {
   const inputRef = useRef("");
   const [apiData, setApiData] = useState("");
+  const [spinner, setSpinner] = useState(false);
 
   const searchCity = async () => {
+    setSpinner(true);
     const URL = `https://api.openweathermap.org/data/2.5/weather?q=${inputRef.current.value}&units=metric&appid=${apiKey}`;
     axios
       .get(URL)
       .then((data) => {
         SuccessNotify();
         setApiData(data);
+        setSpinner(false);
         console.log(data);
       })
       .catch((err) => {
         ErrorNotify();
+        setSpinner(false);
         console.log(err);
       });
   };
@@ -147,26 +158,28 @@ const Search = () => {
         <h2>نام شهر خود را وارد کنید</h2>
       </SearchTitle>
       <SearchBox className="search-box">
-        <button onClick={searchCity} className="search-btn">
-          پیدا کن
+        <button onClick={searchCity}>
+          {spinner ? (
+            <>
+              جست و جو <img src={loadingGif} alt="loading gif" />
+            </>
+          ) : (
+            "پیدا کن"
+          )}
         </button>
         <input
           type="text"
           className="search-bar"
           placeholder="نام شهر به انگلیسی"
           ref={inputRef}
-          autoFocus
         />
       </SearchBox>
 
       {apiData && (
         <SearchData>
           <div>
+            <p>{apiData.data.weather[0].main}</p>
             <h3>{`${apiData.data.name}, ${apiData.data.sys.country}`}</h3>
-            <p>
-              {`${apiData.data.weather[0].main} (${apiData.data.weather[0].description}`}
-              )
-            </p>
             <p>{dateBuilder(new Date())}</p>
           </div>
           <p className="temp">{Math.round(apiData.data.main.temp)}°c</p>
